b392bb18749d55836c5a829267aa766a89985c1c
CorinneSpruit <corinnemeijdam@hotmail.com>
2020-10-29 11:28:24 +0100

    Deel 1, ik ga nu verder met de extra's.

--- src/FilesInlezen.java
+++ src/FilesInlezen.java
@@ -1,3 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FilesInlezen {
+
+    public static void main(String[] arg) {
+        try {
+            File f = new File("Hello.txt");
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                System.out.println(s.nextLine());
+            }
+        } catch (FileNotFoundException a) {
+            System.out.println("Helaas, het bestand kan niet gevonden worden. De reden is: " + a);
+
+        } finally {
+            System.out.println("Het programma is afgelopen");
+        }
+
+
+    }
+
 }
